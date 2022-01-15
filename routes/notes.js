c3a234f2b4009a37605478c13dff62bf4cdad55f
robertsjn2000 <robertsjn2000@gmail.com>
2022-01-14 21:05:32 -0500

    fixed style sheet

--- routes/notes.js
+++ routes/notes.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const router = express.Router();
 function displayNotes(){
-    return JSON.parse(fs.readFileSync('db/db.json'))
+    return JSON.parse(fs.readFileSync('./db/db.json'))
 }
 
-router.post('/notes', (req, res) => {
+router.post('/', (req, res) => {
     const notes = displayNotes()
     const {title, text} = req.body;
     if (req.body){
@@ -17,21 +17,21 @@ router.post('/notes', (req, res) => {
             id: randomUUID(),
         }
         notes.push(newNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(notes));
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
         res.json(newNote)
     }else{
         res.error('No note added.')
     }
 })
-router.get('/notes', (req, res) => {
+router.get('/', (req, res) => {
     const notes = displayNotes()
     res.json(notes);
 })
 
-router.delete('/notes/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const notes = displayNotes();
     const updatedNotes = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(updatedNotes));
     res.json({ok: true})
 })
 
